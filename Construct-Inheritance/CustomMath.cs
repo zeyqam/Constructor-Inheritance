fe10564b9e4ed7a1cafebddbbeaffb577157b09a
zeyqam <zegamda@code.edu.az>
2024-02-23 02:46:35 +0400

    mathoperation task

---
+++ Construct-Inheritance/CustomMath.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construct_Inheritance
+{
+    public class CustomMath
+    {
+        public int SumOfOdds(int[] arr)
+        {
+            int OddSum = 0;
+            foreach (int num in arr)
+            {
+                if(num%2==0)
+                {
+                    OddSum += num;
+                }
+                
+            }
+            return OddSum;
+        }
+        public string CheckEvenOrOdd(int num)
+        {
+            if(num%2==0)
+            {
+                return num + " is an even number";
+            }
+            else
+            {
+                return num + " is an odd number";
+            }
+        }
+        public int SumOfEvenNumbersSquared(int[] arr)
+        {
+            int evenSum = 0;
+            foreach (int num in arr)
+            {
+                if(num%2==0)
+                {
+                    evenSum += num;
+                }
+            }
+            return evenSum * evenSum;
+        }
+        public int Factorial(int n)
+        {
+            int factorialResult = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                factorialResult *= i;
+
+            }
+            return factorialResult;
+        }
+        
+
+           
+
+    }
+}
---
+++ Construct-Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Construct_Inheritance;
+
+CustomMath mathOperations=new CustomMath();
+int[] arr1 = { 1, 2, 3, 4, 5 };
+Console.WriteLine("Sum of odd numbers:" + mathOperations.SumOfOdds(arr1));
+
+int num = 7;
+Console.WriteLine("Number is:" +mathOperations.CheckEvenOrOdd(num));
+
+int[] arr2 = {1,2,3,4,5};
+Console.WriteLine("Sum of squared numbers:" + mathOperations.SumOfEvenNumbersSquared(arr2));
+
+int n= 5;
+Console.WriteLine("Factorial of "  + n + "is" +mathOperations.Factorial(n));
