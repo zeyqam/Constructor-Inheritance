b3f1489e3cf80c09d51e326e780507b590d1b955
zeyqam <zegamda@code.edu.az>
2024-02-23 03:11:22 +0400

    employee task part 1

---
+++ Construct-Inheritance/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construct_Inheritance
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Email { get ; set; }
+        public int Age { get; set; }
+
+
+
+
+    }
+}
--- Construct-Inheritance/Program.cs
+++ Construct-Inheritance/Program.cs
@@ -1,15 +1,35 @@
 ﻿
 using Construct_Inheritance;
 
-CustomMath mathOperations=new CustomMath();
-int[] arr1 = { 1, 2, 3, 4, 5 };
-Console.WriteLine("Sum of odd numbers:" + mathOperations.SumOfOdds(arr1));
+//CustomMath mathOperations=new CustomMath();
+//int[] arr1 = { 1, 2, 3, 4, 5 };
+//Console.WriteLine("Sum of odd numbers:" + mathOperations.SumOfOdds(arr1));
 
-int num = 7;
-Console.WriteLine("Number is:" +mathOperations.CheckEvenOrOdd(num));
+//int num = 7;
+//Console.WriteLine("Number is:" +mathOperations.CheckEvenOrOdd(num));
 
-int[] arr2 = {1,2,3,4,5};
-Console.WriteLine("Sum of squared numbers:" + mathOperations.SumOfEvenNumbersSquared(arr2));
+//int[] arr2 = {1,2,3,4,5};
+//Console.WriteLine("Sum of squared numbers:" + mathOperations.SumOfEvenNumbersSquared(arr2));
 
-int n= 5;
-Console.WriteLine("Factorial of "  + n + "is" +mathOperations.Factorial(n));
+//int n= 5;
+//Console.WriteLine("Factorial of "  + n + "is" +mathOperations.Factorial(n));
+
+Employee[] employees =
+{
+    new Employee{Id=1,Name="Anar",Surname="Abiyev",Address="123 street",Email="anar@gmail.com",Age=25},
+    new Employee{Id=2,Name="Akif",Surname="Adiyev",Address="23 street",Email="akif@gmail.com",Age = 30},
+    new Employee{Id=3,Name="Ayten",Surname="Aliyev",Address="56 street",Email="ayten@gmail.com",Age=22}
+};
+
+void PrintEmployeesOlderThan( int age,Employee [] employees)
+{
+    Console.WriteLine("Yashdan boyuk olan employeeler:");
+    foreach(Employee emp in employees)
+    {
+        if (emp.Age > age)
+        {
+            Console.WriteLine(emp.Name + " " + emp.Surname);
+        }
+    }
+}
+PrintEmployeesOlderThan(25, employees);
