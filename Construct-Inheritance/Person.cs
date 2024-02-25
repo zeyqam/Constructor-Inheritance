067244fd39dc16715de7376157aaf5dbcdf76aa9
zeyqam <zegamda@code.edu.az>
2024-02-26 00:20:44 +0400

    practice

---
+++ Construct-Inheritance/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construct_Inheritance
+{
+    class Person
+    {
+        public int id;
+        public string fullName;
+        public int age;
+        public string position;
+
+        //public Person(int id,string fullName,int age,string position)
+
+        //{
+        //    this.id= id;
+        //    this.fullName = fullName;
+        //    this.age = age;
+        //    this.position = position;
+        //}
+        public Person():this("Sirac")
+        {
+            Console.WriteLine("Hello Behruz bey");
+        }
+        public Person(string data):this(100)
+        {
+            Console.WriteLine(data);
+        }
+        public Person(int num)
+        {
+            Console.WriteLine("num");
+        }
+        public Person(string text,string s):this(text)
+        {
+            Console.WriteLine("Elmir bey");
+            
+        }
+
+
+    }
+}  
+
+    
+
--- Construct-Inheritance/Program.cs
+++ Construct-Inheritance/Program.cs
@@ -14,64 +14,76 @@ using Construct_Inheritance;
 //int n= 5;
 //Console.WriteLine("Factorial of "  + n + "is" +mathOperations.Factorial(n));
 
-Employee[] employees =
-{
-    new Employee{Id=1,Name="Anar",Surname="Abiyev",Address="123 street",Email="anar@gmail.com",Age=25},
-    new Employee{Id=2,Name="Akif",Surname="Adiyev",Address="23 street",Email="akif@gmail.com",Age = 30},
-    new Employee{Id=3,Name="Ayten",Surname="Aliyev",Address="56 street",Email="ayten@gmail.com",Age=22}
-};
+//Employee[] employees =
+//{
+//    new Employee{Id=1,Name="Anar",Surname="Abiyev",Address="123 street",Email="anar@gmail.com",Age=25},
+//    new Employee{Id=2,Name="Akif",Surname="Adiyev",Address="23 street",Email="akif@gmail.com",Age = 30},
+//    new Employee{Id=3,Name="Ayten",Surname="Aliyev",Address="56 street",Email="ayten@gmail.com",Age=22}
+//};
 
-void PrintEmployeesOlderThan( int age,Employee [] employees)
-{
-    Console.WriteLine("Yashdan boyuk olan employeeler:");
-    foreach(Employee emp in employees)
-    {
-        if (emp.Age > age)
-        {
-            Console.WriteLine(emp.Name + " " + emp.Surname);
-        }
-    }
-}
-PrintEmployeesOlderThan(25, employees);
+//void PrintEmployeesOlderThan( int age,Employee [] employees)
+//{
+//    Console.WriteLine("Yashdan boyuk olan employeeler:");
+//    foreach(Employee emp in employees)
+//    {
+//        if (emp.Age > age)
+//        {
+//            Console.WriteLine(emp.Name + " " + emp.Surname);
+//        }
+//    }
+//}
+//PrintEmployeesOlderThan(25, employees);
 
 
-int CountEmplyeeBetweenAges( Employee[] employees, int startAge,int endAge) 
-{int count = 0;
+//int CountEmplyeeBetweenAges( Employee[] employees, int startAge,int endAge) 
+//{int count = 0;
 
-    foreach(Employee emp in employees)
-    {
-        if (emp.Age >= startAge && emp.Age <= endAge)
-        {
-            count++;
-        }
-    }
-    return count;
+//    foreach(Employee emp in employees)
+//    {
+//        if (emp.Age >= startAge && emp.Age <= endAge)
+//        {
+//            count++;
+//        }
+//    }
+//    return count;
 
-}
-int count=CountEmplyeeBetweenAges(employees,20,30);
-Console.WriteLine("20 ve 30 arasinda olan telebelerin sayi: " + count);
+//}
+//int count=CountEmplyeeBetweenAges(employees,20,30);
+//Console.WriteLine("20 ve 30 arasinda olan telebelerin sayi: " + count);
 
-int GetTotalAgeOfEmployees(Employee[] employees)
-{
-    int totalAge=0; 
-    foreach(Employee emp in employees)
-    {
-        totalAge+= emp.Age;
-    }
-    return totalAge;
-}
-int totalAge=GetTotalAgeOfEmployees(employees);
-Console.WriteLine("butun employeelerin yashin cemi: " + totalAge);
+//int GetTotalAgeOfEmployees(Employee[] employees)
+//{
+//    int totalAge=0; 
+//    foreach(Employee emp in employees)
+//    {
+//        totalAge+= emp.Age;
+//    }
+//    return totalAge;
+//}
+//int totalAge=GetTotalAgeOfEmployees(employees);
+//Console.WriteLine("butun employeelerin yashin cemi: " + totalAge);
 
-void PrintEmployeesWithEmailStartingWith(string prefix, Employee[] employees)
-{
-    Console.WriteLine("Emaili'"+prefix +"' ile baslayan employeeler:");
-    foreach(Employee emp in employees)
-    {
-        if (emp.Email.StartsWith(prefix))
-        {
-            Console.WriteLine(emp.Name + " " + emp.Surname + "-" + emp.Email);
-        }
-    }
-}
-PrintEmployeesWithEmailStartingWith("a",employees);
+//void PrintEmployeesWithEmailStartingWith(string prefix, Employee[] employees)
+//{
+//    Console.WriteLine("Emaili'"+prefix +"' ile baslayan employeeler:");
+//    foreach(Employee emp in employees)
+//    {
+//        if (emp.Email.StartsWith(prefix))
+//        {
+//            Console.WriteLine(emp.Name + " " + emp.Surname + "-" + emp.Email);
+//        }
+//    }
+//}
+//PrintEmployeesWithEmailStartingWith("a",employees);
+//Person person = new()
+//{
+//    id = 1,
+//    fullName = "Fatime Qaraxanova",
+//    age = 22,
+//    position = "student"
+//};
+
+//Person person = new(1,"Hacixan Hacixanov",19,"Student");
+//Console.WriteLine(person.fullName);
+Person person = new Person("Azerbaycan","Turkiye");
+
